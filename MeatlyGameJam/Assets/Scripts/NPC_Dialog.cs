34d47c629138303a7761ae8c64eb061eb22cf74a
Scott Schmitz <scottaschmitz@gmail.com>
2015-03-26 22:28:13 -0400

    First commit. Basic animations, reading in of xml dialogue file and player movement.

---
+++ MeatlyGameJam/Assets/Scripts/NPC_Dialog.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+using System.Collections;
+using System.Xml;
+
+public class NPC_Dialog : MonoBehaviour {
+
+	
+	public Text dialogText;
+
+	private bool isTalking = false;	
+	private bool isTextScrolling = false;
+	
+	private string[] talkLines;
+	
+	private int currentLine = 0;
+	private int textSpeed = 2;
+	
+	void Start() {
+	
+		TextAsset textAsset = (TextAsset) Resources.Load ("Dialog");
+	
+		XmlDocument xmlDoc = new XmlDocument();	
+		xmlDoc.LoadXml (textAsset.text);
+	
+		XmlNodeList nodeList = xmlDoc.GetElementsByTagName("scene");
+		
+		foreach (XmlNode sceneNode in nodeList)
+		{
+			// get actors
+			foreach (XmlNode actorNode in sceneNode.ChildNodes)
+			{
+				// Get their lines
+				talkLines = new string[actorNode.ChildNodes.Count];
+				foreach(XmlNode lineNode in actorNode.ChildNodes)
+				{
+					int lineNum = Convert.ToInt16(lineNode.Attributes["id"].Value);
+					talkLines[lineNum] = lineNode.InnerText;
+				}
+			}
+		}
+	}
+	
+	void Update () {
+		if (isTalking) {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				if (isTextScrolling) {
+					// display the full line
+					isTextScrolling = false;
+					dialogText.text = talkLines[currentLine];
+				}
+				else {
+					if (currentLine < talkLines.Length - 1) {
+						currentLine++;
+						isTextScrolling = true;
+						StartCoroutine(scrollText());
+					}
+					else {
+						currentLine = 0;
+						isTalking = false;
+						isTextScrolling = false;
+						
+						dialogText.text = "";
+					}
+				}
+			}
+		}
+	}
+	
+	void OnTriggerEnter2D () {
+		isTalking = true;
+		isTextScrolling = true;
+		
+		StartCoroutine(scrollText());
+	}
+	
+	void OnTriggerExit2D () {
+		 isTalking = false;
+	}
+	
+	IEnumerator scrollText() {
+	
+		string displayText = "";
+		
+		for (int i = 0; i < talkLines[currentLine].Length && isTextScrolling; i++) {
+			displayText += talkLines[currentLine][i];
+			dialogText.text = displayText;
+			yield return new WaitForSeconds(1/textSpeed);		
+		}
+		
+		isTextScrolling = false;
+	}
+}
---
+++ MeatlyGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int movementSpeed = 10;
+	public float jumpForce = 10.0f;
+	
+	private Rigidbody2D myRigidbody;
+	private Animator anim;
+	private int animVelocity;
+	
+	private bool isJumping = false;
+	private bool isGrounded = false;
+	
+	void Awake () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		animVelocity = Animator.StringToHash("Velocity");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (isGrounded) {
+				isJumping = true;
+				isGrounded = false;
+			}
+		}
+	}
+	
+	void FixedUpdate () {
+		float lHorizontal = Input.GetAxisRaw("Horizontal");
+		anim.SetInteger(animVelocity, (int) lHorizontal);
+		
+		myRigidbody.velocity = new Vector2(lHorizontal * movementSpeed, myRigidbody.velocity.y );
+			
+		if(isJumping)
+		{
+			myRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+			isJumping = false;
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D hit) {
+		if(hit.gameObject.tag == "Ground")
+			isGrounded = true;
+	}
+}
